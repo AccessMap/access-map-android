cba45c5c0ac174ac698f0067d6d6236de3ae84ff
sfelker <sfelker@cs.washington.edu>
2015-11-06 12:59:16 -0800

    add zoom buttons, start of api integration

--- app/src/main/java/edu/washington/accessmap/MainActivity.java
+++ app/src/main/java/edu/washington/accessmap/MainActivity.java
@@ -1,8 +1,15 @@
 package edu.washington.accessmap;
 
 import android.app.Dialog;
+import android.app.LoaderManager;
+import android.content.Intent;
 import android.content.IntentSender;
+import android.content.Loader;
+import android.database.Cursor;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -14,6 +21,10 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GooglePlayServicesUtil;
@@ -23,8 +34,22 @@ import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
 import com.mapbox.mapboxsdk.annotations.MarkerOptions;
 import com.mapbox.mapboxsdk.constants.Style;
+import com.mapbox.mapboxsdk.geometry.BoundingBox;
 import com.mapbox.mapboxsdk.geometry.LatLng;
 import com.mapbox.mapboxsdk.views.MapView;
+
+import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.List;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity implements
         GoogleApiClient.ConnectionCallbacks,
@@ -33,10 +58,17 @@ public class MainActivity extends AppCompatActivity implements
 
     private final static int CONNECTION_FAILURE_RESOLUTION_REQUEST = 9000;
     public static final String TAG = MainActivity.class.getSimpleName();
+    private static final int URL_LOADER = 0;
 
     private MapView mapView = null;
+    private EditText addressText = null;
+    private Button searchAddressButton = null;
+    private ImageButton zoomOutButton = null;
+    private ImageButton zoomInButton = null;
+    private List<Address> locationList;
     private GoogleApiClient mGoogleApiClient = null;
     private LocationRequest mLocationRequest = null;
+    private Geocoder geocoder = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,7 +84,104 @@ public class MainActivity extends AppCompatActivity implements
         mapView.setCenterCoordinate(new LatLng(47.6, -122.3));
         mapView.setZoomLevel(11);
         mapView.onCreate(savedInstanceState);
+
+        addressText = (EditText) findViewById(R.id.address_text);
+
+        searchAddressButton = (Button) findViewById(R.id.address_search_button);
+        searchAddressButton.setOnClickListener(searchButtonOnClickListener);
+
+        zoomInButton = (ImageButton) findViewById(R.id.zoom_in_button);
+        zoomInButton.setOnClickListener(zoomInButtonOnClickListener);
+
+        zoomOutButton = (ImageButton) findViewById(R.id.zoom_out_button);
+        zoomOutButton.setOnClickListener(zoomOutButtonOnClickListener);
+
+        geocoder = new Geocoder(this, Locale.ENGLISH);
+
+        // run data queries in background
+        loadSidewalkData();
     }
+
+    View.OnClickListener zoomInButtonOnClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View arg0) {
+            double currentZoomLevel = mapView.getZoomLevel();
+            mapView.setZoomLevel(currentZoomLevel + 1, true);  // animated zoom in
+        }
+    };
+
+    View.OnClickListener zoomOutButtonOnClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View arg0) {
+            double currentZoomLevel = mapView.getZoomLevel();
+            mapView.setZoomLevel(currentZoomLevel - 1, true);  // animated zoom in
+        }
+    };
+
+    View.OnClickListener searchButtonOnClickListener = new View.OnClickListener(){
+        @Override
+        public void onClick(View arg0) {
+            String locationName = addressText.getText().toString();
+
+            Toast.makeText(getApplicationContext(),
+                    "Search for: " + locationName,
+                    Toast.LENGTH_SHORT).show();
+
+            if (locationName == null) {
+                Toast.makeText(getApplicationContext(),
+                        "locationName == null",
+                        Toast.LENGTH_LONG).show();
+            } else {
+                try {
+                    locationList = geocoder.getFromLocationName(locationName, 1);
+
+                    if (locationList == null) {
+                        Toast.makeText(getApplicationContext(),
+                                "locationList == null",
+                                Toast.LENGTH_LONG).show();
+                    } else {
+                        if(locationList.isEmpty()){
+                            Toast.makeText(getApplicationContext(),
+                                    "locationList is empty",
+                                    Toast.LENGTH_LONG).show();
+                        } else {
+                            Toast.makeText(getApplicationContext(),
+                                    "number of result: " + locationList.size(),
+                                    Toast.LENGTH_LONG).show();
+
+                            for (Address a : locationList) {
+                                if (a.getFeatureName() == null) {
+                                    Toast.makeText(getApplicationContext(),
+                                            "search result unknown",
+                                            Toast.LENGTH_LONG).show();
+                                } else {
+                                    Toast.makeText(getApplicationContext(),
+                                            "address resolved to: " + a.getFeatureName(),
+                                            Toast.LENGTH_LONG).show();
+                                }
+
+                                try {
+                                    LatLng searchedPosition = new LatLng(a.getLatitude(), a.getLongitude());
+                                    mapView.addMarker(new MarkerOptions()
+                                            .position(searchedPosition));
+                                    mapView.setCenterCoordinate(searchedPosition, true);  // true animates change
+                                } catch (IllegalStateException ise) {
+                                    Toast.makeText(getApplicationContext(),
+                                            "cannot resolve exact location of searched address",
+                                            Toast.LENGTH_LONG).show();
+                                }
+                            }
+                        }
+                    }
+                } catch (IOException e) {
+                    Toast.makeText(getApplicationContext(),
+                            "network unavailable or any other I/O problem occurs" + locationName,
+                            Toast.LENGTH_LONG).show();
+                    e.printStackTrace();
+                }
+            }
+        }
+    };
 
     protected synchronized void buildGoogleApiClient() {
         mGoogleApiClient = new GoogleApiClient.Builder(this)
@@ -91,8 +220,10 @@ public class MainActivity extends AppCompatActivity implements
         Log.i(TAG, "handling new location");
         double currentLatitude = mLastLocation.getLatitude();
         double currentLongitude = mLastLocation.getLongitude();
+        LatLng currentPosition = new LatLng(currentLatitude, currentLongitude);
         mapView.addMarker(new MarkerOptions()
-                .position(new LatLng(currentLatitude, currentLongitude)));
+                .position(currentPosition));
+        mapView.setCenterCoordinate(currentPosition);
     }
 
     @Override
@@ -160,4 +291,63 @@ public class MainActivity extends AppCompatActivity implements
         super.onSaveInstanceState(outState);
         mapView.onSaveInstanceState(outState);
     }
+
+    public void loadSidewalkData() {
+        String urlString = "@string/api_url" + "/sidewalks.geojson";
+        new CallAccessMapAPI().execute(urlString);
+    }
+
+    private class CallAccessMapAPI extends AsyncTask<String, String, JSONObject> {
+
+        @Override
+        protected JSONObject doInBackground(String... params) {
+            System.out.println("DOING IN BACKGROUND");
+            Log.i(TAG, "DOING IN BACKGROUND");
+            String urlString = params[0];  // URL to call
+
+            JSONObject result = null;
+
+            // HTTP Get
+            try {
+
+                String bbox = new BoundingBox(47.679446, -122.290313, 47.646978, -122.357879).toString();
+                String charset = "UTF-8";
+                String query = String.format("bbox=%s", URLEncoder.encode(bbox, charset));
+
+                URL url = new URL(urlString + "?" + query);
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                BufferedReader in = new BufferedReader( new InputStreamReader(urlConnection.getInputStream()));
+
+                String inputLine;
+                StringBuffer response = new StringBuffer();
+
+                while ((inputLine = in.readLine()) != null) {
+                    response.append(inputLine);
+                }
+
+                in.close();
+
+                System.out.println(response.toString());
+                Log.i(TAG, response.toString());
+
+                result = new JSONObject(response.toString());
+
+            } catch (Exception e ) {
+                System.out.println(e.getMessage());
+                return null;
+            }
+
+            return result;
+        }
+
+        protected void onPostExecute(JSONObject result) {
+            if (result == null) {
+                System.out.println("NULL!!!!");
+                // do nothing
+            } else {
+                Log.i(TAG, "POST EXECUTE!!!");
+                System.out.println("We made it!");
+            }
+        }
+    }  // end CallAccessMapAPI
 }
